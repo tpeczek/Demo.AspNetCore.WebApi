67c16cb5b024136853df5f060bf6b34af18bbbff
Tomasz Pęczek <tpeczek@gmail.com>
2019-01-03 22:22:20 +0100

    Added ApiControllerAtribute, removed FromBodyAttribute, replaced IActionResult with ActionResult<T> where applicable. Resolves #1.

--- Demo.AspNetCore.Mvc.CosmosDB/Controllers/CharactersController.cs
+++ Demo.AspNetCore.Mvc.CosmosDB/Controllers/CharactersController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using MediatR;
@@ -9,11 +8,11 @@ using Demo.AspNetCore.Mvc.CosmosDB.Http;
 using Demo.AspNetCore.Mvc.CosmosDB.Model;
 using Demo.AspNetCore.Mvc.CosmosDB.Requests;
 using Demo.AspNetCore.Mvc.CosmosDB.Exceptions;
-using Microsoft.AspNetCore.JsonPatch;
 
 namespace Demo.AspNetCore.Mvc.CosmosDB.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class CharactersController : Controller
     {
         #region Fields
@@ -37,7 +36,7 @@ namespace Demo.AspNetCore.Mvc.CosmosDB.Controllers
 
         // GET api/characters/{id}
         [HttpGet("{id}")]
-        public async Task<IActionResult> Get(string id)
+        public async Task<ActionResult<Character>> Get(string id)
         {
             Character character = await _mediator.Send(new GetSingleRequest<Character>(id));
             if (character == null)
@@ -45,32 +44,30 @@ namespace Demo.AspNetCore.Mvc.CosmosDB.Controllers
                 return NotFound();
             }
 
-            return new ObjectResult(character);
+            return character;
         }
 
         // POST api/characters
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody]Character character)
+        public async Task<IActionResult> Post(Character character)
         {
-            IActionResult modelValidationResult = await ValidateModelAsync(character);
-            if (modelValidationResult != null)
+            if (await _mediator.Send(new CheckExistsRequest<Character>(character.Name)))
             {
-                return modelValidationResult;
+                return StatusCode((int)HttpStatusCode.Conflict);
             }
 
             character = await _mediator.Send(new CreateRequest<Character>(character));
 
-            return CreatedAtAction(nameof(Get), new { Id = character.Id }, character);
+            return CreatedAtAction(nameof(Get), new { character.Id }, character);
         }
 
         // PUT api/characters/{id}
         [HttpPut("{id}")]
-        public async Task<IActionResult> Put(string id, [FromBody]Character update)
+        public async Task<ActionResult<Character>> Put(string id, Character update)
         {
-            IActionResult modelValidationResult = await ValidateModelAsync(update, id);
-            if (modelValidationResult != null)
+            if (await _mediator.Send(new CheckExistsRequest<Character>(update.Name, id)))
             {
-                return modelValidationResult;
+                return StatusCode((int)HttpStatusCode.Conflict);
             }
 
             Character character = null;
@@ -88,7 +85,7 @@ namespace Demo.AspNetCore.Mvc.CosmosDB.Controllers
                 return NotFound();
             }
 
-            return Ok(character);
+            return character;
         }
 
         // DELETE api/characters/{id}
@@ -108,26 +105,6 @@ namespace Demo.AspNetCore.Mvc.CosmosDB.Controllers
         #endregion
 
         #region Methods
-        private async Task<IActionResult> ValidateModelAsync(Character character, string id = null)
-        {
-            if (character == null)
-            {
-                return BadRequest();
-            }
-
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (await _mediator.Send(new CheckExistsRequest<Character>(character.Name, id)))
-            {
-                return StatusCode((int)HttpStatusCode.Conflict);
-            }
-
-            return null;
-        }
-
         private UpdateRequest<T> CreateUpdateRequest<T>(string id, T update)
         {
             HttpRequestConditions requestConditions = Request.GetRequestConditions();
